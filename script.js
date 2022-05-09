d2d4573b489ae659047a1cef54bf57cfd3b1c34e
Benedetto Simone <sim.benede@gmail.com>
2022-05-09 16:03:50 +0200

    camera on mobile not working

--- script.js
+++ script.js
@@ -56,9 +56,6 @@ camera_button.addEventListener('click', async () => {
     const constraints = {
         video: {
             width: 360, height: 288
-        },
-        facingMode: {
-            exact: 'environment'
         }
     };
 
